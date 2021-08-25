532cb5d0f56c066adb1036d9d6c1cb7986132e62
OriginalCyber <59059908+OriginalCyber@users.noreply.github.com>
2021-08-25 12:24:34 +0700

    v4

--- web/admin.py
+++ web/admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-from web.models import Next, Author
+from web.models import Reporter, Article
+ 
 
-# Register your models here.
+@admin.register(Reporter)
+class ReporterAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name')
 
-# admin.site.register(Next)
-# admin.site.register(Author)
 
-@admin.register(Next)
-class NextAdmin(admin.ModelAdmin):
-    list_display = ['title',  'category', 'date_created']
-
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ['prefix', 'first_name', 'last_name', 'dob']
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['head_line', 'pub_date', 'reporter']
--- web/models.py
+++ web/models.py
@@ -1,58 +1,18 @@
 from django.db import models
-
-CATEGORY_CHOICES = [
-    (1, 'ข่าวประกาศ'),
-    (2, 'โฆษณา'),
-    (3, 'บันทึก'),
-]
-
-PREFIX_CHOICES = [
-    (1, 'นาย'),
-    (2, 'นางสาว'),
-    (3, 'นาง'),
-]
-
-# Create your models here.
+from django.db.models.deletion import CASCADE
 
 
-class Next(models.Model):
-    """Model definition for Next."""
+class Reporter(models.Model):
+    email = models.EmailField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
-    # TODO: Define fields here
-    title = models.CharField(max_length=255)
-    category = models.IntegerField(choices=CATEGORY_CHOICES, default=1)
-    content = models.TextField()
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
-
-
-    class Meta:
-        """Meta definition for Next."""
-
-        verbose_name = 'Next'
-        verbose_name_plural = 'Nexts'
-
-    def __str__(self):
-        """Unicode representation of Next."""
-        return self.title
-
-class Author(models.Model):
-    """Model definition for Author."""
-
-    # TODO: Define fields here
-    prefix = models.IntegerField(choices=PREFIX_CHOICES)
-    first_name = models.CharField(max_length=225)
-    last_name = models.CharField(max_length=225)
-    dob = models.DateField()
-
-
-    class Meta:
-        """Meta definition for Author."""
-
-        verbose_name = 'Author'
-        verbose_name_plural = 'Authors'
-
-    def __str__(self):
-        """Unicode representation of Author."""
+    def __str__(self) -> str:
         return self.first_name + " " + self.last_name
+
+
+class Article(models.Model):
+    head_line = models.CharField(max_length=255)
+    pub_date = models.DateField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
 
